1f7e522ddead3ea5c566a745053a66f17735c46b
SirGoose3432 <jvoelkening@gmail.com>
2016-08-20 07:50:35 -0800

    Removed unneccessary print statement

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main extends Application {
         primaryStage.setTitle("Full Calendar Example");
         primaryStage.setScene(new Scene(new FullCalendarView(YearMonth.now()).getView()));
         primaryStage.show();
-        System.out.println(primaryStage.getWidth() + " " + primaryStage.getHeight());
     }
 
     public static void main(String[] args) {
